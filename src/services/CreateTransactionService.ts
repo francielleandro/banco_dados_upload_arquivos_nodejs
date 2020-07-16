795483dba853f7a5621b043f6437c4ef63861607
Franciel Leandro <francielleandro@gmail.com>
2020-07-16 08:46:47 -0300

    implement post e get

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,30 @@
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import { getCustomRepository } from 'typeorm'; 
+import TranactionRepository from '../repositories/TransactionsRepository';
+import transaction from '../models/Transaction';
+
+interface Request {
+  title:string;
+  type:'income'|'outcome';
+  value:number;
+  category:'string';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({title, value, type, category}:Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TranactionRepository);;
+
+    const transaction = transactionRepository.create({
+      title, 
+      value, 
+      type
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
